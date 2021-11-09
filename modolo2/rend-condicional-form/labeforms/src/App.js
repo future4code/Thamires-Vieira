a5eec628599b240b152cd40b1f4b9480aed4ce3b
Thamires Lippelt Vieira <thamivieira@gmail.com>
2021-11-09 18:12:38 -0300

    consegui fazer a implementação, no desdesafio apenas del todo

---
+++ modolo2/rend-condicional-form/labeforms/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import Etapa1 from "./components/Etapa1";
+import Etapa2 from "./components/Etapa2"
+import Etapa3 from "./components/Etapa3";
+import Final from "./components/Final";
+
+
+
+export default class App extends React.Component {
+  state = {
+    etapa: 1,
+  };
+
+  irParaProximaEtapa = () => {
+    this.setState({
+      etapa: this.state.etapa + 1
+    });
+  };
+
+
+  renderizaEtapa = () => {
+    switch (this.state.etapa){
+      case 1:
+        return <Etapa1 />;
+      case 2:
+        return <Etapa2 />;
+      case 3:
+        return <Etapa3 />;
+      default:
+        return <Final />
+
+    }
+  }
+
+  // deletarBotao = () => {
+  //   if (this.state.etapa === Final){
+      
+  //   }
+  // }
+ 
+  render(){
+   return(
+     <div>
+       {this.renderizaEtapa()}
+       <button onClick={this.irParaProximaEtapa}>Próxima etapa</button>
+     </div>
+   );
+
+ }
+  
+}
+
+
