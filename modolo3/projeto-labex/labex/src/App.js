f9bcb4ddfc311eff49588a0d94d7eea567dcce48
Thamires Lippelt Vieira <thamivieira@gmail.com>
2021-12-07 17:18:28 -0300

    alterei os route para pasta especifica de route

--- modolo3/projeto-labex/labex/src/App.js
+++ modolo3/projeto-labex/labex/src/App.js
@@ -1,46 +1,13 @@
 import React from "react"
-import { Switch } from "react-router"
-import { BrowserRouter, Route } from "react-router-dom"
 import GlobalStyle from "./GlobalStyle"
-import AdminHomePage from "./Pages/AdminHomePage"
-import ApplicationFormPage from "./Pages/ApplicationFormPage"
-import CreateTripPage from "./Pages/CreateTripPage"
-import HomePage from "./Pages/HomePage"
-import ListTripsPage from "./Pages/ListTripsPage"
-import LoginPage from "./Pages/LoginPage"
-import TripDetailsPage from "./Pages/TripDetailsPage"
+import Router from "./route/Router"
 
-const App = () => {
+function App () {
   return (
-    <BrowserRouter>
+    <div>
       <GlobalStyle />
-      <Switch>
-        <Route exact path={"/"}>
-          <HomePage />
-        </Route>
-        <Route exact path={"/trips/list"}>
-          <ListTripsPage />
-        </Route>
-        <Route exact path={"/trips/application"}>
-          <ApplicationFormPage />
-        </Route>
-        <Route exact path={"/login"}>
-          <LoginPage />
-        </Route>
-        <Route exact path={"/admin/trips/list"}>
-          <AdminHomePage />
-        </Route>
-        <Route exact path={"/admin/trips/create"}>
-          <CreateTripPage />
-        </Route>
-        <Route exact path={"/admin/trips/:id"}>
-          <TripDetailsPage />
-        </Route>
-        <Route>
-          <div>Erro 404 - Página não encontrada</div>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+      <Router/>
+    </div>
 
   )
 }
---
+++ modolo3/projeto-labex/labex/src/route/Router.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { Switch } from "react-router"
+import { BrowserRouter, Route } from "react-router-dom"
+import AdminHomePage from "../Pages/AdminHomePage"
+import ApplicationFormPage from "../Pages/ApplicationFormPage"
+import CreateTripPage from "../Pages/CreateTripPage"
+import HomePage from "../Pages/HomePage"
+import ListTripsPage from "../Pages/ListTripsPage"
+import LoginPage from "../Pages/LoginPage"
+import TripDetailsPage from "../Pages/TripDetailsPage"
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={"/"}>
+          <HomePage />
+        </Route>
+        <Route exact path={"/trips/list"}>
+          <ListTripsPage />
+        </Route>
+        <Route exact path={"/trips/application"}>
+          <ApplicationFormPage />
+        </Route>
+        <Route exact path={"/login"}>
+          <LoginPage />
+        </Route>
+        <Route exact path={"/admin/trips/list"}>
+          <AdminHomePage />
+        </Route>
+        <Route exact path={"/admin/trips/create"}>
+          <CreateTripPage />
+        </Route>
+        <Route exact path={"/admin/trips/:id"}>
+          <TripDetailsPage />
+        </Route>
+        <Route>
+          <div>Erro 404 - Página não encontrada</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+    
+  )
+}
+
+export default Router;
