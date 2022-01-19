21189638bc881d1a1f063cb3d81f961f7f25b1c8
Thamires Lippelt <thamivieira@gmail.com>
2022-01-19 20:28:49 -0300

    tentativas

--- modolo4/intro-typescript/template-typescript/src/index.ts
+++ modolo4/intro-typescript/template-typescript/src/index.ts
@@ -52,3 +52,18 @@ function comparaDoisNumeros(num1:number, num2:number):number {
   console.log(comparaDoisNumeros(20,8))
 
   // Exercício 5
+
+//   function checaRenovacaoRG() {
+//     const anoAtual = Number(prompt("Digite o ano atual"))
+//     const anoNascimento = Number(prompt("Digite o ano de nascimento"))
+//     const anoEmissao = Number(prompt("Digite o ano de emissão do documento"))
+ 
+//     const idade = anoAtual - anoNascimento
+//     const tempoCarteira = anoAtual - anoEmissao
+ 
+//     const cond1 = idade <= 20 && tempoCarteira >= 5
+//     const cond2 = idade > 20 && idade <= 50 && tempoCarteira >= 10
+//     const cond3 = idade > 50 && tempoCarteira >= 15
+ 
+//     return (cond1 || cond2 || cond3)
+//  }
