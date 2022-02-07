4864daae327799072c29a67ba75de920744841af
Thamires Lippelt <thamivieira@gmail.com>
2022-02-06 21:27:06 -0300

    inclusão de exerícios

--- modolo4/to-do-list/src/index.ts
+++ modolo4/to-do-list/src/index.ts
@@ -46,6 +46,22 @@ app.get("/user/:id", async (req: Request, res: Response): Promise<void> => {
     }
 })
 
+//Exercício 6 - Pegar todos os usuários
+app.get("/user-all", async (req: Request, res: Response): Promise<void> => {
+    let codeError = 400
+
+    try {
+        const result = await connection("TodoList").select('*')
+        res.status(200).send(result)
+
+    } catch (error: any) {
+        res.status(codeError).send(error.message || error.sqlMessage)
+    }
+})
+
+// Exercício 5 - Pegar Tarefa
+
+
 // Exercício 1 - Criar usuário
 app.post("/user", async (req: Request, res: Response): Promise<void> => {
     let codeError = 400
@@ -73,31 +89,68 @@ app.post("/user", async (req: Request, res: Response): Promise<void> => {
     }
 })
 
-// Exercício 3 - Editar usuário
-app.put("/user/edit/:id", async (req: Request, res: Response): Promise<void> =>{
+// Exercício 4 - Criar Tarefa
+app.post("/task", async (req: Request, res: Response): Promise<void> => {
     let codeError = 400
-  try {
-    const id = req.params.id
-    const { name, nickname, email }: { name: string, nickname: string, email: string } = req.body
+    try {
+        const { title, description, limitDate, creatorUserId }: { title: string, description: string, limitDate: string, creatorUserId: string } = req.body
+        const taskId: string = Date.now().toString()
 
-    if (!id) {
-        codeError = 404
-        throw new Error("Informe um id válido")
+        if (!title || !description || !limitDate || !creatorUserId) {
+            codeError = 422
+            throw new Error("Informe todos os dados necessários")
+        }
+
+        const newDate = (date: string) => {
+            const correctDate = date.split("/")
+            return correctDate[2] + "-" + correctDate[1] + "-" + correctDate[0] as string
+        }
+
+        const correctDate = newDate(limitDate)
+
+        const result = await connection("TodoListTask")
+            .insert({
+                id: taskId,
+                title: title,
+                description: description,
+                limit_date: correctDate,
+                creator_user_id: creatorUserId
+            })
+
+        res.status(201).send("Tarefa criada com sucesso!")
+
+    } catch (error: any) {
+        res.status(codeError).send({ result: error.message || error.sqlMessage })
     }
 
-    if (nickname === "" || name === "" || email === "") {
-      codeError = 404
-      throw new Error("Preecha todos os campos")
+})
+
+
+// Exercício 3 - Editar usuário
+app.put("/user/edit/:id", async (req: Request, res: Response): Promise<void> => {
+    let codeError = 400
+    try {
+        const id = req.params.id
+        const { name, nickname, email }: { name: string, nickname: string, email: string } = req.body
+
+        if (!id) {
+            codeError = 404
+            throw new Error("Informe um id válido")
+        }
+
+        if (nickname === "" || name === "" || email === "") {
+            codeError = 404
+            throw new Error("Preecha todos os campos")
+        }
+
+        await connection("TodoList")
+            .update({ name, nickname, email })
+            .where("id", id)
+
+        res.status(201).send("Alterações feitas com sucesso!")
+
+    } catch (error: any) {
+        res.status(codeError).send(error.message || error.sqlMessage)
+
     }
-
-    await connection("TodoList")
-      .update({ name, nickname, email })
-      .where("id", id)
-
-    res.status(201).send("Alterações feitas com sucesso!")
-
-  } catch (error: any) {
-    res.status(codeError).send(error.message || error.sqlMessage)
-
-  }
 })
