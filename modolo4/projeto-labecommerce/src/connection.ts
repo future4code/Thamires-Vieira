ca0c6c4df3f4a46e22c4c2e672d0868b04478522
Thamires Lippelt <thamivieira@gmail.com>
2022-02-13 23:30:44 -0300

    adicionando lista de produtos

--- modolo4/projeto-labecommerce/src/connection.ts
+++ modolo4/projeto-labecommerce/src/connection.ts
@@ -10,6 +10,6 @@ export const connection = knex({
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_SCHEMA
   }
 })
